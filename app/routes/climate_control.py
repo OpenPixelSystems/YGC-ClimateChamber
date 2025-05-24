7b9a9beef8db2a6fbf9dae3d0363f5c14d6a1051
bryanvdb <bryanvandenberghe@hotmail.com>
2025-05-24 20:36:37 +0200

    implement manual steering

--- app/routes/climate_control.py
+++ app/routes/climate_control.py
@@ -51,3 +51,14 @@ def stop_sensors():
     app_state.start_time = None
     app_state.database.stop_logging_cycle()
     return jsonify({'status': 'Sensors stopped'})
+
+@climate_bp.route('/update-power', methods=['POST'])
+def update_power():
+    """Manually steer peltier power"""
+    data = request.get_json(silent=True) or {}
+    power_value = data.get('power')
+
+    app_state = get_app_state()
+    app_state.climate_chamber.apply_control({"pid_output": power_value})
+
+    return jsonify({'status': 'Peltier power updated'})
